7a5e38308fb39be39ac15bf56ee85f7c921445df
i_can_code_maybe <matej.bican01@gmail.com>
2022-01-31 18:46:06 +0100

    Added glfw3 and created window

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <vector>
+
+#include <GLFW/glfw3.h>
 
 #include <Little.h>
-
-#include <vector>
 
 void ok();
 int main(){
     SaySomething("cusik");
+
+    glfwInit();
+
+    if(!glfwInit())
+        std::cout << "glfw was not initialized \n";
+    
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "wow okno", nullptr, nullptr);
+
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
